c4184e0c449c5e9a798f671083f2b5c377633677
dali lewis <Dalimlg3@hotmail.com>
2018-08-09 16:42:00 -0400

    Mapped Out Player

---
+++ Adventure/Player.cpp
@@ -0,0 +1,79 @@
+#include "Player.h"
+
+player::player(std::string name, std::string race, int level, int exp)
+{
+	setName(name);
+	setRace(race);
+	setLevel(level);
+	setExp(exp);
+	setMaxHealth();
+	setHealth(pMH);
+	setDPS();
+	setExpReq();
+}
+void player::setName(std::string name)
+{
+	pName = name;
+}
+void player::setRace(std::string race)
+{
+	pRace = race;
+}
+void player::setLevel(int lvl)
+{
+	lvl = 1;
+	pLvl = lvl;
+}
+void player::setHealth(double h) 
+{
+	pH = h;
+}
+void player::setMaxHealth()
+{
+	pMH = (10 * getLevel());
+}
+void player::setDPS()
+{
+	pDPS = (2 * getLevel());
+}
+void player::setExp(double exp)
+{
+	exp = 0;
+	Exp = exp;
+}
+void player::setExpReq()
+{
+	ExpReq = 50 + ((getLevel()*getLevel()) * 10);
+}
+std::string player::getName()
+{
+	return pName;
+}
+std::string player::getRace()
+{
+	return pRace;
+}
+int player::getLevel()
+{
+	return pLvl;
+}
+double player::getHealth()
+{
+	return pH;
+}
+double player::getMaxHealth()
+{
+	return pMH;
+}
+int player::getDPS()
+{
+	return pDPS;
+}
+int player::getExp()
+{
+	return Exp;
+}
+int player::getExpReq()
+{
+	return ExpReq;
+}
--- Adventure/Player.h
+++ Adventure/Player.h
@@ -1,1 +1,34 @@
 #pragma once
+#include <string>
+class player
+{
+	// The player class and its defination
+	// Made int Exp a capital E to help me in future in the cpp file, it came in handy
+public:
+	player(std::string, std::string, int, int);
+	void setName(std::string);
+	void setRace(std::string);
+	void setLevel(int);
+	void setExp(double);
+	void setExpReq();
+	void setHealth(double);
+	void setMaxHealth();
+	void setDPS();
+	std::string getName();
+	std::string getRace();
+	int getLevel();
+	double getHealth();
+	double getMaxHealth();
+	int getDPS();
+	int getExp();
+	int getExpReq();
+private:
+	std::string pName;
+	std::string pRace;
+	int pLvl;
+	double pH;
+	double pMH;
+	int pDPS;
+	int Exp;
+	int ExpReq;
+};
